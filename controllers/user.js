c9fa8ab63776d3aec1579b36c3a1882cd6f28246
sm343 <sanjeevmallick2.com>
2025-01-22 18:42:39 +0530

    jwt added

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
-const {v4:uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const User = require("../models/user");
-const {setUser,getUser} = require("../service/auth");
+const { setUser, getUser } = require("../service/auth");
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
   await User.create({ name, email, password });
@@ -12,9 +12,8 @@ async function handleUserLogin(req, res) {
   const user = await User.findOne({ email, password });
   console.log("User", user);
   if (!user) return res.render("login", { error: "Invalid email or password" });
-  const sessionId=uuidv4();
-  setUser(sessionId,user);
-  res.cookie("uid",sessionId);
+  const token = setUser(user);
+  res.cookie("uid", token);
   res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,22 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+const jwt = require("jsonwebtoken");
+const secret = "sanjeev123%";
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
-module.exports = {setUser,getUser};
+module.exports = { setUser, getUser };
